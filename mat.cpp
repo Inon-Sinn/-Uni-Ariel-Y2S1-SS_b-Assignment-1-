5e5b73a0a52a4d5a9e53a48627ad52bfcc9a4c47
Inon Sinn <inon.sinn@gmail.com>
2022-03-08 02:39:55 +0200

    compiles for now

---
+++ mat.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include"mat.hpp"
+
+using namespace std;
+
+namespace ariel { 
+    std::string mat(int length, int breath, char sym_one, char sym_two){
+        cout << "test" << endl;
+        return "test";
+    };
+};
